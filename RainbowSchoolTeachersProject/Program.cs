2597b8fb2aeab681e7bbdba8634910bb455aea90
Rajesh B <rajesh_b2@dell.com>
2021-11-01 18:55:57 +0530

    Pushing the code

---
+++ RainbowSchoolTeachersProject/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace RainbowSchoolTeachersProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = 1;
+            string answer = "n";
+            do
+            {
+                try
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("*******************************************************");
+                    Console.WriteLine("Welcome to Teacher Database!!");
+                    Console.WriteLine("*******************************************************");
+                    Console.WriteLine("1. Insert");
+                    Console.WriteLine("2. Update");
+                    Console.WriteLine("3. Retrieve by Teacher Id");
+                    Console.WriteLine("4. Retrieve All Teachers");
+                    Console.WriteLine("5. Exit");
+                    Console.WriteLine("*******************************************************");
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+
+                    Console.Write("Enter Option: ");
+
+                    input = Convert.ToInt32(Console.ReadLine());
+                    switch (input)
+                    {
+                        case 1:
+
+                            Console.WriteLine("Insert Operation");
+
+                            do
+                            {
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                Teacher.Insert();
+                                Console.Write("Do you want to Insert more records? (y/n): ");
+                                answer = Console.ReadLine().ToLower();
+                            } while (answer == "y");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.WriteLine("-------------------------------------------------------");
+                            break;
+                        case 2:
+                            do
+                            {
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                Console.WriteLine("Update Operation");
+                                Teacher.Update();
+                                Console.Write("Do you want to Update more records? (y/n): ");
+                                answer = Console.ReadLine().ToLower();
+                            } while (answer == "y");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.WriteLine("-------------------------------------------------------");
+                            break;
+                        case 3:
+                            do
+                            {
+                                Console.Write("Enter Teacher ID:");
+                                int Id = Convert.ToInt32(Console.ReadLine());
+                                Teacher t =  Teacher.GetTeacherById(Id);
+                                bool isExist = Teacher.DisplayTeacher(t);
+                                Console.Write("Do you want view other teacher record? (y/n): ");
+                                answer = Console.ReadLine().ToLower();
+                            } while (answer == "y");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.WriteLine("-------------------------------------------------------");
+                            break;
+                        case 4:
+                            Teacher.GetAllTeachers();
+                            break;
+                        case 5:
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.WriteLine("Exit Operation");
+                            break;
+                        default:
+                            Console.ForegroundColor = ConsoleColor.DarkRed;
+                            Console.WriteLine("Choose an valid option(1 to 5)");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            break;
+                    }
+                }
+                catch (FormatException ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("Enter only number for Teacher ID.");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+
+            } while (input != 5);
+        }        
+    }
+}
---
+++ RainbowSchoolTeachersProject/Teacher.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace RainbowSchoolTeachersProject
+{
+    [Serializable]
+    internal class Teacher
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Class { get; set; }
+
+        public string Section { get; set; }
+
+        private static string fileName = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName + "\\Data\\Teachers_Data.txt";
+        
+        public Teacher()
+        {
+
+        }
+
+        #region Methods
+
+        public static void Serialize(List<Teacher> teacher)
+        {
+            string jsonString = JsonSerializer.Serialize(teacher);
+            //File.Open(fileName, FileMode.Append);
+            File.WriteAllText(fileName,jsonString);
+        }
+
+
+        public static List<Teacher> Deserialize()
+        {
+            List<Teacher> Teacher = new List<Teacher>();
+            try
+            {
+                string jsonString = File.ReadAllText(fileName);
+                Teacher = JsonSerializer.Deserialize<List<Teacher>>(jsonString);
+                
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("Teacher Records text file does not exist.");
+
+            }            
+            return Teacher;
+        }
+
+        static List<Teacher> TeacherList = new();
+
+        public static void Insert()
+        {
+            TeacherList = Deserialize();
+            Teacher teacher = new();
+            Console.WriteLine("Enter the Teacher Details: ");
+            Console.Write("ID : ");
+            teacher.ID = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Name : ");
+            teacher.Name = Console.ReadLine();
+            Console.Write("Class : ");
+            teacher.Class = Console.ReadLine();
+            Console.Write("Section : ");
+            teacher.Section = Console.ReadLine();
+
+            TeacherList.Add(teacher);
+            Teacher.Serialize(TeacherList);
+            Console.WriteLine("Insert operation successful.");
+
+        }
+
+        public static void Update()
+        {
+            try
+            {
+                Console.WriteLine("Enter the Teacher ID which you want to update:");
+                Console.Write("Teacher ID: ");
+                int ID = Convert.ToInt32(Console.ReadLine());
+                Teacher t = GetTeacherById(ID);
+                bool isExist = DisplayTeacher(t);
+                if (isExist)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+
+                    Console.WriteLine("Enter the Teacher details to update:");
+
+                    Console.Write("Teacher Name : ");
+                    string Name = Console.ReadLine();
+                    Console.Write("Class : ");
+                    string Class = Console.ReadLine();
+                    Console.Write("Section : ");
+                    string Section = Console.ReadLine();
+
+                    foreach (Teacher teacher in TeacherList)
+                    {
+                        if (teacher.ID == ID)
+                        {
+                            teacher.Name = Name;
+                            teacher.Class = Class;
+                            teacher.Section = Section;
+                            break;
+                        }
+                    }
+                    Teacher.Serialize(TeacherList);
+                    Console.WriteLine("Update operation successful.");
+                    Console.ForegroundColor = ConsoleColor.White;
+
+                }
+                //else
+                //{
+                //    Console.WriteLine("Teacher ID entered not found in the records.");
+
+                //}
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("ID entered not in Number format, " + ex.Message );
+            }
+        }
+
+        public static void GetAllTeachers()
+        {
+            List<Teacher> Teacher = Deserialize();
+
+            if (Teacher.Count> 0)
+            {
+
+                Console.WriteLine("The following are the list of all teachers.");
+
+                foreach (Teacher teacher in Teacher)
+                {
+                    Console.WriteLine("Teacher ID : " + teacher.ID.ToString());
+                    Console.WriteLine("Teacher Name : " + teacher.Name);
+                    Console.WriteLine("Teacher Class : " + teacher.Class);
+                    Console.WriteLine("Teacher Section : " + teacher.Section);
+                    Console.WriteLine("-------------------------------------------------------");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No teachers found.");
+            }
+
+        }
+
+        public static Teacher GetTeacherById(int TeacherID)
+        {
+            List<Teacher> Teacher = Deserialize();
+            Teacher teacher = Teacher.FirstOrDefault(x => x.ID == TeacherID);
+            return teacher;
+            
+        }
+
+        public static bool DisplayTeacher(Teacher teacher)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+
+            if (teacher != null)
+            {
+                Console.WriteLine("-------------------------------------------------------");
+                Console.WriteLine("Teacher Details for Teacher ID: " + teacher.ID.ToString());
+                Console.WriteLine("-------------------------------------------------------");
+                Console.WriteLine("Teacher ID: " + teacher.ID.ToString());
+
+                Console.WriteLine("Teacher Name : " + teacher.Name);
+                Console.WriteLine("Teacher Class : " + teacher.Class);
+                Console.WriteLine("Teacher Section : " + teacher.Section);
+                Console.ForegroundColor = ConsoleColor.White;
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Teacher ID entered is not found in the records.");
+                Console.ForegroundColor = ConsoleColor.White;
+                return false;
+            }
+        }
+        #endregion Methods
+    }
+}
